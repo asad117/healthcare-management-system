ccabf07d41673ba391093015bfb1efa10a2d1cf6
kafeel-hub <115660180+kafeel-hub@users.noreply.github.com>
2024-08-04 12:49:12 +0530

    minor change

--- src/components/AppointmentCalendar.jsx
+++ src/components/AppointmentCalendar.jsx
@@ -1,15 +1,10 @@
 // src/components/AppointmentCalendar.jsx
-import React from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const AppointmentCalendar = ({ onDateChange }) => {
-  return (
-    <Calendar 
-      onChange={onDateChange} 
-      minDetail="month" // to limit view to month and below
-    />
-  );
+  return <Calendar onChange={onDateChange} minDetail="month" />;
 };
 
 export default AppointmentCalendar;
